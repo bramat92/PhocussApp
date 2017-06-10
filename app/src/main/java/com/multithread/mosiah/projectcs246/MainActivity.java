b46ea08b5d55f60c8f2ff28b8d60f1edd16fdd4b
dunnapey <pjdunnaway@gmail.com>
2017-06-09 19:28:52 -0600

    added second activity and onclick intent

--- app/src/main/java/com/multithread/mosiah/projectcs246/MainActivity.java
+++ app/src/main/java/com/multithread/mosiah/projectcs246/MainActivity.java
@@ -1,13 +1,33 @@
 package com.multithread.mosiah.projectcs246;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.addTask);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EditTask.class));
+
+                Intent intent
+            }
+        });
+
+        Toast.makeText(getApplication(), "OnCreate called!", Toast.LENGTH_LONG).show();
     }
 }
