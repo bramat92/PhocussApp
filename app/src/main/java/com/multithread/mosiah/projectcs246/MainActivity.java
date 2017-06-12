eccaa1fabe61d2bf48231f2d36fa65638d913447
mosiah10Z <que14003@byui.edu>
2017-06-12 15:22:07 -0600

    edited Edit task to have a edit view

--- app/src/main/java/com/multithread/mosiah/projectcs246/MainActivity.java
+++ app/src/main/java/com/multithread/mosiah/projectcs246/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private Button button;
 
