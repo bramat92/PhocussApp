685ca3c9e33eb421bfae32791cdd7f69e4c40f46
dunnapey <pjdunnaway@gmail.com>
2017-07-13 04:42:57 -0600

    Added reset button to streak tally activity and attempted to add font change to tally inside code, but doesn't work yet (deprecated font handling?).

--- app/src/main/java/com/multithread/mosiah/projectcs246/TallyActivity.java
+++ app/src/main/java/com/multithread/mosiah/projectcs246/TallyActivity.java
@@ -3,12 +3,15 @@ package com.multithread.mosiah.projectcs246;
 import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import static com.multithread.mosiah.projectcs246.R.string.tally;
 
 /**
  * <p>TallyActivity displays the goal completion streak for the user.</p>
@@ -20,6 +23,8 @@ public class TallyActivity extends AppCompatActivity {
     private int tallyCount;
     private TextView tallyView;
     Button backButton;
+    Button reset;
+    private SharedPreferences tally;
 
     /**
      * Just added a back button to UI
@@ -31,6 +36,13 @@ public class TallyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tally);
 
+        //this should alter textView to different font, but doesn't work. Deprecated?
+//        Typeface font = Typeface.createFromAsset(getAssets(),"@fonts/digital-7");
+//        tallyView.setTypeface(font);
+
+        tallyView = (TextView) findViewById(R.id.tallyAnimation);
+        tally = getSharedPreferences("tallyCount", MODE_PRIVATE);
+
         backButton = (Button) findViewById(R.id.backToMain);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +50,16 @@ public class TallyActivity extends AppCompatActivity {
                 //this brings us back to main activity
                 Intent intent = new Intent(TallyActivity.this, MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        reset = (Button) findViewById(R.id.buttonReset);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tally.edit().putInt("tally", 0).commit();
+                tallyCount = tally.getInt("tally", 0);
+                animateTextView(0, tallyCount, tallyView);
             }
         });
     }
@@ -49,18 +71,8 @@ public class TallyActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        getTaskTally();
-    }
-
-    /**
-     * Gets tallyCount from sharedPreferences and displays it in a quick animated up-counter
-     * @author Peyton Dunnaway
-     */
-    public void getTaskTally() {
-        //access shared preferences tallyCount
-        SharedPreferences tally = getSharedPreferences("tallyCount", MODE_PRIVATE);
         tallyCount = tally.getInt("tally", 0);
-        animateTextView(0, tallyCount, tallyView = (TextView)findViewById(R.id.tallyAnimation));
+        animateTextView(0, tallyCount, tallyView);
     }
 
     /**
@@ -73,7 +85,7 @@ public class TallyActivity extends AppCompatActivity {
     public void animateTextView(int initialValue, int finalValue, final TextView textview) {
 
         ValueAnimator valueAnimator = ValueAnimator.ofInt(initialValue, finalValue);
-        valueAnimator.setDuration(2000);
+        valueAnimator.setDuration(1500);
 
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
